8a060568d1fc5d77bb3b720a1bec3ed1ce4349f9
Srishti.Sinha <srishtisinha03@gmail.com>
2019-07-30 15:05:32 +0530

    Added Alert system

---
+++ AlertSystem/AlertImpl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Patient;
+
+namespace AlertSystem
+{
+    public class AlertImpl : IAlert
+    {
+        public void Alert(string patient_id)
+        {
+            Console.Write($"Patient No. {patient_Id} has abnormal values !!!");
+        }
+    }
+}
---
+++ AlertSystem/IAlert.cs
@@ -0,0 +1,9 @@
+﻿using Patient;
+
+namespace AlertSystem
+{
+    public interface IAlert 
+    {
+        void Alert(string patient_id);
+    }
+}
